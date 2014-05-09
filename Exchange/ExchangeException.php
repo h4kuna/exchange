db5a0bccb1fb89010320df5e945edb67281a0725
Milan Matejcek <milan.matejcek@marketvision.cz>
2014-05-09 16:07:42 +0200

    New namespace

---
+++ Exchange/ExchangeException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace h4kuna\Exchange;
+
+use RuntimeException;
+
+/**
+ * Exchange exception
+ */
+final class ExchangeException extends RuntimeException {
+    
+}
---
+++ Exchange/Utils.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace h4kuna\Exchange;
+
+use Nette\StaticClassException;
+
+/**
+ * Static helpers
+ *
+ * @author Milan Matějček
+ */
+class Utils {
+
+    public function __construct() {
+        throw new StaticClassException;
+    }
+
+    /**
+     * Stroke replace by point
+     *
+     * @param string $str
+     * @return string
+     */
+    public static function stroke2point($str) {
+        return trim(str_replace(',', '.', $str));
+    }
+
+}
