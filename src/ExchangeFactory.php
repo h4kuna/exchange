428cece86b45d67c066f447ce0d3d55d8a39a501
Milan Matějček <milan.matejcek@gmail.com>
2024-06-04 12:44:12 +0200

    feat(ExchangeFactory): improve dependency on ClientInterface

--- src/ExchangeFactory.php
+++ src/ExchangeFactory.php
@@ -25,20 +25,26 @@ final class ExchangeFactory implements ExchangeFactoryInterface
 		private ?string $to = null,
 		?RatingListCache $ratingListCache = null,
 		array $allowedCurrencies = [],
+		?ClientInterface $client = null,
+		?RequestFactoryInterface $requestFactory = null,
 	)
 	{
-		$this->ratingListCache = $ratingListCache ?? self::createRatingListCache(Utils::transformCurrencies($allowedCurrencies));
+		$this->ratingListCache = $ratingListCache ?? self::createRatingListCache(Utils::transformCurrencies($allowedCurrencies), $client, $requestFactory);
 	}
 
 
 	/**
 	 * @param array<string, int> $allowedCurrencies
 	 */
-	private static function createRatingListCache(array $allowedCurrencies): RatingListCache
+	private static function createRatingListCache(
+		array $allowedCurrencies,
+		?ClientInterface $client,
+		?RequestFactoryInterface $requestFactory
+	): RatingListCache
 	{
 		return new RatingListCache(
 			self::createCacheFactory()->create(),
-			new SourceDownload(self::createClient(), self::createRequestFactory(), $allowedCurrencies),
+			new SourceDownload($client ?? self::createClient(), $requestFactory ?? self::createRequestFactory(), $allowedCurrencies),
 		);
 	}
 
